5d820ee867455853bad71872678bc73142ea4214
Silvina Figueroa <silvina.sfigueroa@gmail.com>
2024-05-16 16:06:13 -0700

    create students object and calculate assigment scores

--- fundamentals.mjs
+++ fundamentals.mjs
@@ -8,37 +8,62 @@ import { CourseInfo, AssignmentGroup, LearnerSubmissions } from "./data.mjs"
 
 getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions)
 
+
+
+
+
+
 function getLearnerData(course, assig, learnerSub) {
-
+    let result = [];
     for (let i in learnerSub) {
-        let learner_id = learnerSub[i].learner_id
-        let assignment_id = learnerSub[i].assignment_id
-        let submitted = learnerSub.submission
-        let dateSubmission = learnerSub[i].submission.dateSubmission;
-        let score = learnerSub[i].submission.score;
+        let learnerId = learnerSub[i].learner_id;
+        let assigmentId = learnerSub[i].assignment_id;
+        let percentage = (getAssignment(assigmentId, learnerSub[i].submission.score, learnerSub[i].submission.dateSubmission))
+        result.push({"id":learnerId, [`<${assigmentId}>`] : percentage})
 
-        console.log(assignmentInfo(assignment_id))
-        
     }
+     return console.log(result)
+
 }
 
-// Get the assigment due date and points possible
-function assignmentInfo(assig) {
-    let dueDate = "";
-    let points= 0;
-    AssignmentGroup.assignments.forEach(element => {
-        // console.log(`${element.id} & ${assig}`) //validation
-        if (element.id === assig) {
-            dueDate = element.due_at
-            points = element.points_possible
+
+function getAssignment(assignmentId, score, dateSubmission) {
+    let dueAt = "";
+    let pointsPossible = 0;
+        // Get assigment due data and points Possible
+        for (let assig of AssignmentGroup.assignments) {
+            if (assig.id == assignmentId) {
+                dueAt = assig.due_at
+                pointsPossible = assig.points_possible
+                break; // Assigments has unique ID, so if we found it we don't need to continue looking on the array
+            }
         }
-       
-    })
+        let today = new Date();
+        //Check if the assigment is due
+        if(today > dueAt){
+            return "Exam no yet due!" // Using return to exit the whole function
+        }
+        // Validate if the assigment belong to the course
+        let course = validateCourse(AssignmentGroup)
+        console.log(course)
 
-    return [dueDate, points]
-};
+        // Get assigment score %
+        let percentage = getScore(score,pointsPossible)
+        // Apply penalty if assigment submited late! 
+        return (dueAt < dateSubmission) ? (percentage - 10) : percentage
+
+}
 
 
+//Calculate the score in % -- TODO: Add validations (division by 0 - catch error)
+function getScore(score,totalPoints){
+    let scorePerc = (score / totalPoints).toFixed(2)
+    return scorePerc
+}
 
 
-
+function validateCourse(assigGroup) {
+    if (assigGroup.course_id == CourseInfo.id) {
+        return "Go to go!"
+    }
+}
